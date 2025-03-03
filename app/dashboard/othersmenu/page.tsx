2b86005b92b9099890c9439cc742f7682ded5491
dicky hung <72612942+dickyhung@users.noreply.github.com>
2025-03-04 05:36:43 +0700

    update import lusitana

---
+++ app/dashboard/othersmenu/page.tsx
@@ -0,0 +1,3 @@
+export default function OthersMenuPage() {
+  return <p>Others menu Page</p>;
+}
--- app/ui/customers/table.tsx
+++ app/ui/customers/table.tsx
@@ -1,10 +1,12 @@
-import Image from 'next/image';
-import { lusitana } from '@/app/ui/fonts';
-import Search from '@/app/ui/search';
+import Image from "next/image";
+import { lusitana } from "@/app/ui/font";
+import Search from "@/app/ui/search";
 import {
   CustomersTableType,
   FormattedCustomersTable,
-} from '@/app/lib/definitions';
+} from "@/app/lib/definitions";
+
+// update dimas
 
 export default async function CustomersTable({
   customers,
